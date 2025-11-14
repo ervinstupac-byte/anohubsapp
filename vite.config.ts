09925fbf2803d449fa0d4715a8b5da91f961964e
Amazing Clam <ervinstupac@gmail.com>
2025-11-14 16:34:53 +0100

    ee

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,15 @@
-import path from 'path';
+// vite.config.ts
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// TAČAN NAZIV VAŠEG REPOZITORIJUMA!
-const repoName = '/anohubsapp/'; 
-
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    // KLJUČNO: Postavljamo base na RELATIVNU putanju ('.')
     return {
-        // OVO JE DODATNI BASE PATH KOJI REŠAVA GITHUB PAGES PROBLEM
-        base: repoName, 
+        base: './', 
         
-        server: {
-            port: 3000,
-            host: '0.0.0.0',
-        },
+        // ... (Ostale konfiguracije)
+        
         plugins: [react()],
-        define: {
-            'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-            'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-        },
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, '.'),
-            }
-        }
+        // ...
     };
 });
