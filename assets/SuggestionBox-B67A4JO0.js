fbb10cd0b82731b1a3a150a841bf9e2aed21287b
Amazing Clam <ervinstupac@gmail.com>
2025-12-14 12:59:48 +0100

    Updates

--- assets/SuggestionBox-B67A4JO0.js
+++ assets/SuggestionBox-B67A4JO0.js
@@ -1,4 +1,4 @@
-import{r as i,j as e}from"./index-CDCh0vTJ.js";import{B as c}from"./BackButton-CknuqbGY.js";const u=()=>{const[t,o]=i.useState({title:"",category:"Process Improvement",description:"",email:""}),l=s=>{s.preventDefault();const r=`AnoHub Idea: ${t.title} (${t.category})`,n=`
+import{r as i,j as e}from"./index-BsLWT1BY.js";import{B as c}from"./BackButton-CfdfQ24I.js";const u=()=>{const[t,o]=i.useState({title:"",category:"Process Improvement",description:"",email:""}),l=s=>{s.preventDefault();const r=`AnoHub Idea: ${t.title} (${t.category})`,n=`
 Hello AnoHub Team (ino@anohubs.com),
 
 I have a suggestion for the platform:
