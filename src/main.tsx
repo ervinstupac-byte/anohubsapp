13b760333266666c093d30a0b4f2fcb1eafc9dbc
Amazing Clam <ervinstupac@gmail.com>
2025-12-16 10:26:53 +0100

    v6

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,7 @@ import App from './App.tsx';
 import './index.css'; // Tvoj globalni dizajn (Glassmorphism, Tailwind)
 import './i18n/index.ts'; // Inicijalizacija prijevoda
 
-// --- IMPORT PROVIDERS ---
-import { AuthProvider } from './contexts/AuthContext.tsx';
-import { ToastProvider } from './contexts/ToastContext.tsx';
-import { AssetProvider } from './contexts/AssetContext.tsx';
-import { TelemetryProvider } from './contexts/TelemetryContext.tsx'; // <--- NOVI "MOTOR"
-import { RiskProvider } from './contexts/RiskContext.tsx';
-import { QuestionnaireProvider } from './contexts/QuestionnaireContext.tsx';
+// --- IMPORT PROVIDERS REMOVED (Moved to App.tsx) ---
 
 const rootElement = document.getElementById('root');
 
@@ -22,33 +16,6 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    {/* 1. AUTH PROVIDER (Najvažniji, provjerava korisnika) */}
-    <AuthProvider>
-      
-      {/* 2. TOAST PROVIDER (Da obavijesti rade svugdje) */}
-      <ToastProvider>
-        
-        {/* 3. ASSET PROVIDER (Učitava turbine iz baze) */}
-        <AssetProvider>
-          
-          {/* 4. TELEMETRY PROVIDER (Generira Live Podatke) */}
-          {/* VAŽNO: Mora biti UNUTAR AssetProvidera jer koristi popis imovine */}
-          <TelemetryProvider>
-            
-            {/* 5. LOGIC PROVIDERS (Rizik i Upitnik) */}
-            <RiskProvider>
-              <QuestionnaireProvider>
-                
-                {/* 6. GLAVNA APLIKACIJA */}
-                <App />
-                
-              </QuestionnaireProvider>
-            </RiskProvider>
-
-          </TelemetryProvider>
-          
-        </AssetProvider>
-      </ToastProvider>
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
