2fd8a9a476a6211ea21bfc65caed53236af222e6
Amazing Clam <ervinstupac@gmail.com>
2025-12-16 11:55:30 +0100

    Updates

--- assets/RiverWildlife-DlYNOtUy.js
+++ assets/RiverWildlife-DlYNOtUy.js
@@ -1,4 +1,4 @@
-import{b as c,d as o,j as e}from"./index-CtE2iAbl.js";import{r as x}from"./vendor-BLMQF4_i.js";import{B as m}from"./BackButton-C_LzzzHs.js";import{T as h}from"./Trans-D5wgYbb5.js";import"./pdf-C099XNqT.js";const b=({item:s,isOpen:i,onClick:a,delay:r})=>e.jsxs("div",{className:`
+import{b as c,e as o,j as e}from"./index-D3i9LJzx.js";import{r as x}from"./vendor-BLMQF4_i.js";import{B as m}from"./BackButton-B1mgtFDV.js";import{T as h}from"./Trans-Av3RwQmN.js";import"./pdf-C099XNqT.js";const b=({item:s,isOpen:i,onClick:a,delay:r})=>e.jsxs("div",{className:`
             group relative overflow-hidden rounded-2xl transition-all duration-500 animate-fade-in-up
             ${i?"bg-slate-800/80 ring-1 ring-cyan-500/50 shadow-lg":"bg-slate-900/40 border border-white/5 hover:border-white/10 hover:bg-slate-800/60"}
             backdrop-blur-md
