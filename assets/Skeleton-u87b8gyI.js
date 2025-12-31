34a98bcaf0f24f3e61822c9077b292b4a810d698
Amazing Clam <ervinstupac@gmail.com>
2025-12-31 18:17:39 +0100

    Updates

--- assets/Skeleton-u87b8gyI.js
+++
@@ -1,5 +0,0 @@
-import{j as r}from"./ui-BJ3qj9Yb.js";const o=({className:e="",width:s,height:t,circle:l=!1})=>r.jsx("div",{className:`
-                animate-pulse bg-slate-800/50 
-                ${l?"rounded-full":"rounded-lg"} 
-                ${e}
-            `,style:{width:s||"100%",height:t||"1rem"}});export{o as S};
