4db863e447dd4fe4fed8426295ac28480fd18e0f
Amazing Clam <ervinstupac@gmail.com>
2026-01-04 00:06:04 +0100

    Updates

---
+++ assets/Skeleton-CgNCZwzu.js
@@ -0,0 +1,5 @@
+import{j as r}from"./ui-D7oYvBZX.js";const o=({className:e="",width:s,height:t,circle:l=!1})=>r.jsx("div",{className:`
+                animate-pulse bg-slate-800/50 
+                ${l?"rounded-full":"rounded-lg"} 
+                ${e}
+            `,style:{width:s||"100%",height:t||"1rem"}});export{o as S};
